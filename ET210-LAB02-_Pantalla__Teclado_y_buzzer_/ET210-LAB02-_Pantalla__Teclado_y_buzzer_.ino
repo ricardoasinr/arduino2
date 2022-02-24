846566c341d8a275f6eb93e91184e4702b592a87
Ricardo Asin <ricardoasinr@gmail.com>
2022-02-24 10:45:38 -0400

    Buzzer y pantalla

---
+++ ET210-LAB02-_Pantalla__Teclado_y_buzzer_/ET210-LAB02-_Pantalla__Teclado_y_buzzer_.ino
@@ -0,0 +1,114 @@
+
+//////////////////////////////////////////////////////
+#include <Adafruit_GFX.h>   // libreria para pantallas graficas
+#include <Adafruit_SSD1306.h>   // libreria para controlador SSD1306 
+#define ANCHO 128     // reemplaza ocurrencia de ANCHO por 128
+#define ALTO 64       // reemplaza ocurrencia de ALTO por 64
+#define OLED_RESET 4      // necesario por la libreria pero no usado
+Adafruit_SSD1306 oled(ANCHO, ALTO, &Wire, OLED_RESET);  // crea objeto
+//////////////////////////////////////////////////////
+#define buzzer_pin 2
+///////////////////////////////////////////////////////
+
+#include <Keypad.h> // importa o incluye la libreria Keypad
+const byte FILAS=4;
+const byte COLUMNAS=4;
+char keys[FILAS][COLUMNAS]= {    // define la distribucion de teclas
+  {'1','2','3','A'},
+  {'4','5','6','B'}, 
+  {'7','8','9','C'}, 
+  {'*','0','#','D'} 
+};
+byte pinesFilas[FILAS]= {13,12,11,10};
+byte pinesColumnas[COLUMNAS]= {9,8,7,6};
+Keypad teclado = Keypad(makeKeymap(keys), pinesFilas, pinesColumnas, FILAS, COLUMNAS);
+char tecla;
+///////////////////////////////////////////////////////
+
+
+void setup() { 
+  //////////////////////////////////////
+  Wire.begin();         // inicializa bus I2C
+  oled.begin(SSD1306_SWITCHCAPVCC, 0x3C); // inicializa pantalla con direccion 0x3C
+  oled.clearDisplay();
+  oled.display();
+  //////////////////////////////////////
+  Serial.begin(9600);    // inicializa comunicacion serie a 9600 bps
+ 
+  ///////////////////////////////////////////////////////
+  pinMode(buzzer_pin, OUTPUT);
+  ///////////////////////////////////////////////////////
+
+}
+ 
+void loop() {
+ 
+// PRUEBA DE BUZZER PASIVO
+//tone(buzzer_pin, 1523);
+//  delay(200);
+//  noTone(buzzer_pin);
+//  delay(2000); 
+
+// PRUEBA DE TECLADO MATRICIAL Y PANTALLA OLED
+ tecla= teclado.getKey(); // obtiene tecla presionada y asigna el valor a TECLA  
+  if (tecla)
+  {
+    Serial.println(tecla);
+    delay(10);
+    switch (tecla)
+    {
+      case '*':       
+        oled.clearDisplay();      
+        oled.setTextColor(WHITE);   
+        oled.setCursor(0, 0);     
+        oled.setTextSize(2);
+        oled.print("1");
+        tone(buzzer_pin, 1523);
+        delay(200);
+  noTone(buzzer_pin);
+delay(200); 
+        oled.display();
+        //delay(200);
+        break;
+        
+      case '0':
+        oled.clearDisplay();      
+        oled.setTextColor(WHITE);   
+        oled.setCursor(0, 0);     
+        oled.setTextSize(2);      
+        oled.print("2");
+        oled.display();
+        //delay(200);
+        break;
+        
+      case '#':
+        oled.clearDisplay();      
+        oled.setTextColor(WHITE);   
+        oled.setCursor(0, 0);     
+        oled.setTextSize(2);      
+        oled.print("4");
+        oled.display();
+        //delay(200);
+        break;
+
+      case 'D':
+        oled.clearDisplay();      
+        oled.setTextColor(WHITE);   
+        oled.setCursor(0, 0);     
+        oled.setTextSize(2);      
+        oled.print("5");
+         delay(200); 
+          tone(buzzer_pin, 1523); 
+        noTone(buzzer_pin);
+      delay(500);  
+        oled.display();
+        //delay(200);
+        break;
+      default: 
+        break;      
+    }
+   } 
+
+
+
+}
