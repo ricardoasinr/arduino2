5fd57dd4483ebee4eff5cb2cddbd4284557f928b
Ricardo Asin <ricardoasinr@gmail.com>
2022-04-20 18:02:55 -0400

    Reloj

---
+++ Laboratorios/programa_2/programa_2.ino
@@ -0,0 +1,75 @@
+/*
+  Capitulo 38 de Arduino desde cero en Español.
+  Programa que establece una alarma para escribir en monitor serie el texto Alarma
+  exactamente a las 14:30 hs. todos los dias.
+
+  Autor: bitwiseAr  
+
+*/
+
+#include <Wire.h>   // incluye libreria para interfaz I2C
+#include <RTClib.h>   // incluye libreria para el manejo del modulo RTC
+
+RTC_DS3231 rtc;     // crea objeto del tipo RTC_DS3231
+
+bool alarma = true;   // variable de control con valor verdadero
+
+void setup () {
+ Serial.begin(9600);    // inicializa comunicacion serie a 9600 bps
+ pinMode(13, OUTPUT);
+
+ 
+ if (! rtc.begin()) {       // si falla la inicializacion del modulo
+ Serial.println("Modulo RTC no encontrado !");  // muestra mensaje de error
+ while (1);         // bucle infinito que detiene ejecucion del programa
+ }
+ rtc.adjust(DateTime(__DATE__, __TIME__));
+}
+
+void loop () {
+ 
+ digitalWrite(13, LOW);
+
+ 
+rtc.adjust(DateTime(2022, 2, 3, 9, 30, 0));
+DateTime fecha = rtc.now();        // funcion que devuelve fecha y horario en formato
+              // DateTime y asigna a variable fecha
+ if ( fecha.hour() == 12 && fecha.minute() == 0 && fecha.second() == 10){ // si hora = 14 y minutos = 30
+   // if ( alarma == true ){        // si alarma tiene un valor verdadero
+      
+      Serial.println( "Alarma activada" ); 
+      digitalWrite(13,HIGH);
+      
+      // imprime en monitor serie texto Alarma
+   //   alarma = false;         // carga valor falso en variable de control
+  //  }             // para evitar ingresar mas de una vez
+  }
+
+  
+ Serial.print(fecha.day());       // funcion que obtiene el dia de la fecha completa
+ Serial.print("/");         // caracter barra como separador
+ Serial.print(fecha.month());       // funcion que obtiene el mes de la fecha completa
+ Serial.print("/");         // caracter barra como separador
+ Serial.print(fecha.year());        // funcion que obtiene el año de la fecha completa
+ Serial.print(" ");         // caracter espacio en blanco como separador
+ Serial.print(fecha.hour());        // funcion que obtiene la hora de la fecha completa
+ Serial.print(":");         // caracter dos puntos como separador
+ Serial.print(fecha.minute());        // funcion que obtiene los minutos de la fecha completa
+ Serial.print(":");         // caracter dos puntos como separador
+ Serial.println(fecha.second());      // funcion que obtiene los segundos de la fecha completa
+ 
+ delay(1000);           // demora de 1 segundo
+
+
+
+ if ( fecha.hour() == 12 && fecha.minute() == 0 && fecha.second() == 20){ // si hora = 14 y minutos = 30
+   // if ( alarma == true ){        // si alarma tiene un valor verdadero
+      
+      Serial.println( "Alarma Desactivada" ); 
+      digitalWrite(13,LOW);
+      
+      // imprime en monitor serie texto Alarma
+   //   alarma = false;         // carga valor falso en variable de control
+  //  }             // para evitar ingresar mas de una vez
+  } 
+}
