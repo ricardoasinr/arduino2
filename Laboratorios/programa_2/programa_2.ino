79c8ac146b2e73ff3ac99c63d66806a755d27d3e
Ricardo Asin <ricardoasinr@gmail.com>
2022-04-20 22:51:22 -0400

    Reloj corregido

--- Laboratorios/programa_2/programa_2.ino
+++ Laboratorios/programa_2/programa_2.ino
@@ -9,6 +9,7 @@
 
 #include <Wire.h>   // incluye libreria para interfaz I2C
 #include <RTClib.h>   // incluye libreria para el manejo del modulo RTC
+int ledverde_pin=13;
 
 RTC_DS3231 rtc;     // crea objeto del tipo RTC_DS3231
 
@@ -16,36 +17,36 @@ bool alarma = true;   // variable de control con valor verdadero
 
 void setup () {
  Serial.begin(9600);    // inicializa comunicacion serie a 9600 bps
- pinMode(13, OUTPUT);
+ pinMode(ledverde_pin, OUTPUT);
 
- 
  if (! rtc.begin()) {       // si falla la inicializacion del modulo
  Serial.println("Modulo RTC no encontrado !");  // muestra mensaje de error
  while (1);         // bucle infinito que detiene ejecucion del programa
  }
- rtc.adjust(DateTime(__DATE__, __TIME__));
+
+ rtc.adjust(DateTime(2022, 4, 3, 19, 25, 55));
+ //rtc.adjust(DateTime(__DATE__, __TIME__));
 }
 
 void loop () {
- 
- digitalWrite(13, LOW);
-
- 
-rtc.adjust(DateTime(2022, 2, 3, 9, 30, 0));
-DateTime fecha = rtc.now();        // funcion que devuelve fecha y horario en formato
+ DateTime fecha = rtc.now();        // funcion que devuelve fecha y horario en formato
               // DateTime y asigna a variable fecha
- if ( fecha.hour() == 12 && fecha.minute() == 0 && fecha.second() == 10){ // si hora = 14 y minutos = 30
+ if ( fecha.hour() == 19 && fecha.minute() == 26 && fecha.second() == 0){ // si hora = 14 y minutos = 30
    // if ( alarma == true ){        // si alarma tiene un valor verdadero
-      
-      Serial.println( "Alarma activada" ); 
+      Serial.println( "Alarma" );// imprime en monitor serie texto Alarma
       digitalWrite(13,HIGH);
       
-      // imprime en monitor serie texto Alarma
    //   alarma = false;         // carga valor falso en variable de control
   //  }             // para evitar ingresar mas de una vez
   }
-
-  
+   if ( fecha.hour() == 19 && fecha.minute() == 26 && fecha.second() == 5){ // si hora = 14 y minutos = 30
+   // if ( alarma == true ){        // si alarma tiene un valor verdadero
+      Serial.println( "Alarma desactivada" );// imprime en monitor serie texto Alarma
+      digitalWrite(13,LOW);
+      
+   //   alarma = false;         // carga valor falso en variable de control
+  //  }             // para evitar ingresar mas de una vez
+  }
  Serial.print(fecha.day());       // funcion que obtiene el dia de la fecha completa
  Serial.print("/");         // caracter barra como separador
  Serial.print(fecha.month());       // funcion que obtiene el mes de la fecha completa
@@ -60,16 +61,9 @@ DateTime fecha = rtc.now();        // funcion que devuelve fecha y horario en fo
  
  delay(1000);           // demora de 1 segundo
 
+  //if ( fecha.hour() == 19 && fecha.minute() == 25 ) {  // si hora = 2 y minutos = 0 restablece valor de
+    
+  //}
+ //   alarma = true;          // variable de control en verdadero 
 
-
- if ( fecha.hour() == 12 && fecha.minute() == 0 && fecha.second() == 20){ // si hora = 14 y minutos = 30
-   // if ( alarma == true ){        // si alarma tiene un valor verdadero
-      
-      Serial.println( "Alarma Desactivada" ); 
-      digitalWrite(13,LOW);
-      
-      // imprime en monitor serie texto Alarma
-   //   alarma = false;         // carga valor falso en variable de control
-  //  }             // para evitar ingresar mas de una vez
-  } 
 }
